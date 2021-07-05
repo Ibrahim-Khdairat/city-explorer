1d4f8ae964e0543017a7f19d6f9e3e81711e7ad2
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-07-05 19:58:45 +0300

    lab-07 Weather Information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import CityCard from './components/CityCard';
 import MapModal from './components/MapModal';
+import WeatherCard from './components/WeatherCard';
+import Error from './components/Error';
 import './App.css'
 
 
@@ -15,7 +17,10 @@ class App extends React.Component {
       cityName: '',
       cityInformation: {},
       showInformation: false,
-      showMap: false
+      showMap: false,
+      WeatherInformation: [],
+      showWeather: false,
+      showError: false
     }
   }
 
@@ -35,7 +40,32 @@ class App extends React.Component {
       cityInformation: responseData.data[0],
       showInformation: true,
     })
+
+
+    // This for weather information 
+    this.renderWeather();
   }
+
+
+
+  renderWeather = async () => {
+    const city = this.state.cityName.charAt(0).toUpperCase() + this.state.cityName.slice(1);
+
+    let weatherUrl = `https://city-explorer-backend-301d25.herokuapp.com/weatherinfo?cityName=${city}&format=json`;
+
+    let weatherData = await axios.get(weatherUrl)
+    await this.setState({
+      WeatherInformation: weatherData.data,
+      showWeather: true,
+    })
+
+    console.log(this.state.WeatherInformation.map(day => day.description))
+
+  }
+
+
+
+
   // This function response to show the map modal
   showMapModal = async () => {
     await this.setState({
@@ -51,30 +81,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className = "main" >
-        <div className = "form">
-        <h1>City Explorer</h1>
-        <p>In this page you can search for any city and show the details for it as City Name , Latitude, and Longitude</p>
+      <div className="main" >
+        <div className="form">
+          <h1>City Explorer</h1>
+          <p>In this page you can search for any city and show the details for it as City Name , Latitude, and Longitude</p>
 
-        <form onSubmit={this.exploreCity}>
-          <input type="text" placeholder="Name of the city" name="city" />
-          <button type="submit"> Explore </button>
-        </form>
+          <form onSubmit={this.exploreCity} >
+            <input type="text" placeholder="Name of the city" name="city" />
+            <button type="submit"> Explore </button>
+          </form>
         </div>
 
+        {/* <p>{this.state.WeatherInformation.map (day => day.description) } </p> */}
+
+        {/* <Error showError={this.state.showError} /> */}
         <CityCard cityInformation={this.state.cityInformation} showInformation={this.state.showInformation} showMapModal={this.showMapModal} />
+
+        <WeatherCard WeatherInformation={this.state.WeatherInformation} showWeather={this.state.showWeather} cityInformation={this.state.cityInformation} renderWeather={this.renderWeather} />
 
         <MapModal cityInformation={this.state.cityInformation} showMap={this.state.showMap} handleClose={this.handleClose} />
 
 
-        
-        {/* <h3>City Name : {this.state.cityInformation.display_name}</h3>
-        <h3>Latitude : {this.state.cityInformation.lat}</h3>
-        <h3>Longitude : {this.state.cityInformation.lon}</h3>
 
-        {this.state.showInformation &&
-          <img alt='' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityInformation.lat},${this.state.cityInformation.lon}&zoom=15`} />
-        } */}
+
 
 
       </div>
